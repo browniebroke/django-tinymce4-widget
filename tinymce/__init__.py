ca662cbc383f8014fc2f65b4a85a92d2fb6ebdcd
Bruno Alla <browniebroke@users.noreply.github.com>
2020-09-21 17:55:24 +0100

    chore: use declarative config in setup.cfg

--- tinymce/__init__.py
+++ tinymce/__init__.py
@@ -12,6 +12,5 @@ for Django forms and models.
 from .models import HTMLField
 from .widgets import AdminTinyMCE, TinyMCE
 
+__version__ = "4.0.0"
 __all__ = ["HTMLField", "TinyMCE", "AdminTinyMCE"]
-
-VERSION = "4.0.0"
