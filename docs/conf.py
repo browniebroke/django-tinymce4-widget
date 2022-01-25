050aecec934ec3b7d5147c02f91d05da57eceb15
Bruno Alla <alla.brunoo@gmail.com>
2022-01-25 00:25:58 +0000

    fixup! refactor: move testing code into a tests packaqe

--- docs/conf.py
+++ docs/conf.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath(".."))
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "test_tinymce.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tests.settings")
 
 import tinymce  # noqa E402 isort:skip
 
